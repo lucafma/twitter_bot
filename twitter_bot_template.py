7ac832f3cf377cc705905286848860784c94cf25
Luca F. Marques <lucafma@rc.unesp.br>
2017-06-24 16:59:38 -0300

    tests every 15 minutes

--- twitter_bot_template.py
+++ twitter_bot_template.py
@@ -1,23 +1,27 @@
-import tweepy, subprocess, re, time, datetime, csv
+import tweepy, datetime, csv
+from re import findall
+from subprocess import getoutput
+from time import time, sleep
 
-CONSUMER_KEY = "JS2gvGmrlLNVcD5pUzwsFFQ9B"
-CONSUMER_SECRET = "57rgFgECQq09SkIwZ8DFVgl4fbXARuaJNnfO3CXfDMTXGnvmA3"
-ACCESS_TOKEN = "1213856437-Uo2D2VVizTTm5RjHU3J6Y25KvrIwVjMFp3Pf0Lo"
-ACCESS_SECRET = "xC1c9dtLOTU9qblNgIeAzyPuEBOB0QHpTMuNxF4nwD0Hg"
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+ACCESS_TOKEN = ""
+ACCESS_SECRET = ""
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
 
 def test_speed():
-	speeds = subprocess.getoutput(['speedtest-cli', '--simple'])
-	print("Test completed.")
-	return re.findall(r'(\d+\.\d+)', speeds)
+	print("Testing...")
+	speeds = getoutput(['speedtest-cli', '--simple'])
+	print("\nTest completed.")
+	return findall(r'(\d+\.\d+)', speeds)
 
 def format_data(data, down_speed, up_speed, isp, cidade):
 	if float(data[0]) > 100:
 		return (1, "Ei {}, por que meu ping está {} ms aqui em {}".format(isp, data[0]), cidade)
-	elif float(data[1]) < down_speed*0.7 or float(data[2]) < up_speed*0.7: 
+	elif float(data[1]) < down_speed*0.75 or float(data[2]) < up_speed*0.75: 
 		return (1, "Ei {}, por que minha internet está em {1}down e {2}up quando pago por {1}down/{2}up? Aqui em {}".format(isp, data[1], data[2], cidade))
 	return (0, "Ok.")
 
@@ -42,20 +46,22 @@ def input_handle():
 	return (down, up, isp_handle, cidade)
 
 def spreadsheet_output(data):
-	time_stamp = time.time()
+	time_stamp = time()
 	date = datetime.datetime.fromtimestamp(time_stamp).strftime('%Y-%m-%d %H:%M:%S')
-	out_file = open("data.csv", 'a')
+	out_file = open("data.csv", 'a+')
 	writer = csv.writer(out_file)
-	writer.writerow((date, (time_stamp*1000), isp, data[0], data[1], data[2]))
+	writer.writerow((isp, date, data[0], data[1], data[2]))
 	out_file.close()
 
 if __name__ == '__main__':
 	down, up, isp, cidade = input_handle()
-	data = test_speed()
-	spreadsheet_output(data)
-	print("Ping: {}\nDownload: {}\nUpload: {}".format(data[0], data[1], data[2]))
-	status, tweet = format_data(data, down, up, isp, cidade)
-	if(status):
-		send_tweet(tweet)
-	else:
-		print("Internet normal.")
+	while(1):
+		data = test_speed()
+		spreadsheet_output(data)
+		print("Ping: {}\nDownload: {}\nUpload: {}".format(data[0], data[1], data[2]))
+		status, tweet = format_data(data, down, up, isp, cidade)
+		if(status):
+			send_tweet(tweet)
+		else:
+			print("Internet normal.")
+		sleep(900)
